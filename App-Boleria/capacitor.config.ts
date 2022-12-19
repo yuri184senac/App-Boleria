84c4c0b649be1c8719c61ee9170655a8d2389694
Técnico em Informática 2021.2 <36127512021.2@senacrjedu.local>
2022-12-19 10:19:37 -0300

    APP BOLERIA

---
+++ App-Boleria/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'App-Boleria',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
